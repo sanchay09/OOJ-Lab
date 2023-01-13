05e0c4dd763ea3343dfa07bcc3f3bdc11fce133b
sanchay09 <50539725+sanchay09@users.noreply.github.com>
2023-01-13 15:36:55 +0530

    Lab Programs (13-01-2023)
    Lab Programs (13-01-2023)

---
+++ Lab Programs (13-01-2023)/Threads/Multithreading.java
@@ -0,0 +1,58 @@
+/*Write a program which creates two threads, one thread displaying “BMS College of Engineering” once every ten seconds 
+and another displaying “CSE” once every two seconds.*/
+
+import java.util.Scanner;
+
+class BMSCE extends Thread {
+	synchronized public void run() 
+	{
+		try 
+		{
+			int i=0;
+			while (i<5) 
+			{
+				sleep(10000);
+				System.out.println("BMS College of Engineering ");
+				i++;
+			}
+		} 
+
+		catch (Exception e) {
+		}
+	System.out.println("Exiting Thread 1");
+	}
+}
+
+class CSE extends Thread 
+	{
+	synchronized public void run() 
+	{
+		try 
+		{
+			int i=0;
+			while (i<5) 
+			{
+				sleep(2000);
+				System.out.println("CSE");
+				i++;
+			}
+		} 
+		
+		catch (Exception e) {
+		}
+	System.out.println("Exiting Thread 2");
+
+	}
+}
+
+
+class Multithreading 
+{
+	public static void main(String args[]) 
+	{
+		BMSCE t1 = new BMSCE();
+		CSE t2 = new CSE();
+		t1.start();
+		t2.start();
+	}
+}
